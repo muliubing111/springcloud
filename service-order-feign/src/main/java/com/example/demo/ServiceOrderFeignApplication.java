2d06247e691ad731b39014b2b011986e99ddfd9b
dingfuzhao <630211041@qq.com>
2019-04-16 10:27:27 +0800

    cloud版本从1.0升级到2.0

--- service-order-feign/src/main/java/com/example/demo/ServiceOrderFeignApplication.java
+++ service-order-feign/src/main/java/com/example/demo/ServiceOrderFeignApplication.java
@@ -3,11 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 @EnableFeignClients
 @EnableHystrix
 public class ServiceOrderFeignApplication {
--- service-order-feign/src/main/java/com/example/demo/service/MemberFeign.java
+++ service-order-feign/src/main/java/com/example/demo/service/MemberFeign.java
@@ -1,7 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.service.impl.MemberFeignService;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;

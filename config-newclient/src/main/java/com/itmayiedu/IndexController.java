1cecb4d73ea2646fb4d2a85adb9c1a22697a2013
dingfuzhao <630211041@qq.com>
2019-04-23 14:24:26 +0800

    修改application.yml名为bootstrap.yml，同时手动刷新

--- config-newclient/src/main/java/com/itmayiedu/IndexController.java
+++ config-newclient/src/main/java/com/itmayiedu/IndexController.java
@@ -1,17 +1,19 @@
 package com.itmayiedu;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class IndexController {
+	@Value("${age}")
+	String age;
 
-	@Value("${name}")
-	private String name;
-
-	@RequestMapping("/name")
-	private String name() {
-		return name;
+	//使用手动刷新,加RefreshScope注解,@Value参数 不能是name/userName之类的,怀疑是关键字(此时加注解启动后调接口返回空)
+	@RequestMapping("/getAge")
+	private String getAge() {
+		return age;
 	}
 }
